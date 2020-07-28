03a6a4d31cff06c663c4cb3a3ca38e64b4c7abac
jonesPersonal <2243319805@qq.com>
2020-07-28 16:06:19 +0800

    update value of x

--- src/com/wkzf/test/CircleTest.java
+++ src/com/wkzf/test/CircleTest.java
@@ -3,7 +3,7 @@ package com.wkzf.test;
 public class CircleTest {
     public static void main(String[] args)
     {
-        int x=1;
+        int x=12;
         while(x<20)
         {
             System.out.println("value of x:" + x);
