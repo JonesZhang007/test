7ab0470fea502fe6c4df915b53bee37b67593c36
jonesPersonal <2243319805@qq.com>
2020-07-28 16:00:14 +0800

    add circle:while

---
+++ src/com/wkzf/test/CircleTest.java
@@ -0,0 +1,14 @@
+package com.wkzf.test;
+
+public class CircleTest {
+    public static void main(String[] args)
+    {
+        int x=1;
+        while(x<20)
+        {
+            System.out.println("value of x:" + x);
+            x++;
+            System.out.println("\n");
+        }
+    }
+}
