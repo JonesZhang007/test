fb192422dd23a7f610f94544ca0f1ddced0fd234
jonesPersonal <2243319805@qq.com>
2020-07-28 16:14:16 +0800

    update x thirdly

--- src/com/wkzf/test/CircleTest.java
+++ src/com/wkzf/test/CircleTest.java
@@ -3,7 +3,7 @@ package com.wkzf.test;
 public class CircleTest {
     public static void main(String[] args)
     {
-        int x=3;
+        int x=9;
         while(x<20)
         {
             System.out.println("value of x:" + x);
