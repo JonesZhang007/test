d2d68be267958bceb40ae96114fc0997badbe7aa
jonesPersonal <2243319805@qq.com>
2020-07-31 23:09:21 +0800

    add @ 23:08

--- src/com/wkzf/test/ITTest.java
+++ src/com/wkzf/test/ITTest.java
@@ -5,6 +5,9 @@ import org.omg.CORBA.INTERNAL;
 class ITTest {
     public static void main(String[] args)
     {
+        int [] ar = {1,2,3,4,5};
+        System.out.println(ar[4]);
+
         int[] arr={1,32,23,44,11};
         ChangeIT.DoIT(arr);
         for(int i=0;i<arr.length;i++)
